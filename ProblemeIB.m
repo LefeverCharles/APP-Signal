eb010529d271791037e4ae68fea30ae55f544927
plguillou <44848881+plguillou@users.noreply.github.com>
2019-12-09 19:49:42 +0100

    Update ProblemeIB.m

--- ProblemeIB.m
+++ ProblemeIB.m
@@ -6,10 +6,10 @@ f = 130*((2^(1/12))^(12*5 - 1))*(1 + 0.02*rand(1,1));    % rand(nombre d'élemen
 phi =  2*pi.*rand(1,1);              % génère aléatoirement un déphasage aléatoire
 
 # Qu'ils aient le même nombre de points
-longEchantillon = 100 * max(1/fref, 1/f);
+longEchantillon = 10/fref;
 x =  0 : 1/16000 : longEchantillon;
 
-f = fref * (2^(1/12));
+f = fref / (2^(1/12))
 signal1 = Aref*sin(2*pi*fref*x);
 signal2 = A*sin(2*pi*f*x + phi);
 
@@ -18,35 +18,29 @@ signal2 = A*sin(2*pi*f*x + phi);
 signal1Norm = signal1 / max(signal1);
 signal2Norm = signal2 / max(signal2);
 
-# On va faire une fonction produit scalaire légèrement modifiée
-# On prend la sqrt pour que la comparaison ne dépende pas beaucoup
-# de la hauteur de la fréquence car si on fait 4000*4000 ça sera non lineairement
-# plus grand que 130 * 130 
-# abs( ) nous assure de pas avoir de complexes
-function out = my_dot(x, y)
-  out = 0;
-  for i = 1 : 1 : length(x)
-    out += sqrt(abs(x(i)*y(i)));
-  end
-end
+frequenceComparaison = 2*round(length(x) * 0.2); # 2/10
 
-frequenceComparaison = round(length(x) / 5);
-
+disp('Calculating autoCorr...')
 # Autocorrelation : Somme du carré des éléments
-autoCorr = my_dot(signal1Norm(1:frequenceComparaison), signal1Norm(1:frequenceComparaison)); # Produit scalaire
+autoCorr = dot(signal1Norm(1:frequenceComparaison), signal1Norm(1:frequenceComparaison)); # Produit scalaire
 
 # Intercorrelation
 maxiCorr = 0;
+disp('Calculating interCorr...')
 for k = 1 : 1 : frequenceComparaison
-  temp = my_dot(signal1Norm(1:frequenceComparaison), signal2Norm(k + 1 : frequenceComparaison + k));
+  temp = dot(signal1Norm(1 : frequenceComparaison), signal2Norm(k + 1 : frequenceComparaison + k));
   if (temp > maxiCorr)
     maxiCorr = temp;
   end
 end
 
-pourcentageCorr = maxiCorr / autoCorr;
+disp('Calculating pourcentageCorr...')
+pourcentageCorr = sqrt(maxiCorr / autoCorr);
 
-if (pourcentageCorr > 0.5)
+disp('Resultat :')
+# Pour f = fref */ 2^(1/12) on est souvent au dessus de 0.90
+# On met donc le seuil à 0.89
+if (pourcentageCorr > 0.89)
   disp(1);
 else
   disp(0);
