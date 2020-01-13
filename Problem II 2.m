1d170cccce861967ddcc4a02f15943b2076ef9e2
plguillou <44848881+plguillou@users.noreply.github.com>
2020-01-13 11:06:05 +0100

    Update Problem II 2.m

--- Problem II 2.m
+++ Problem II 2.m
@@ -37,9 +37,45 @@ end
 end
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%% 2e algo %%% (celui du screen)
 
-function [x,Fe] = Open_dat(filename) %Fonction que la prof nous donne
+clear all;
+close all;
+clc;
+
+[signal,Fe] = Open_dat('ECG/101.dat'); % Ne pas oublier Open_dat.m avec matlab
+longueurFenetre = 3; % secondes
+longueurFichier = size(signal);
+longueurFichier = longueurFichier(2);
+longueurFichier = longueurFichier / Fe; % en secondes
+pasFenetres = 0.5;%longueurFenetre / 5;
+
+for i = 0:pasFenetres:(longueurFichier - longueurFenetre)
+    
+  signalFenetre = signal(1, round(i*Fe) + 1 : round((i + longueurFenetre)*Fe));
+  signalFenetre = signalFenetre - mean(signalFenetre);
+  sizeSignalFenetre = size(signalFenetre);
+  sizeSignalFenetre = sizeSignalFenetre(2);
+  signal_fft = abs(fft(signalFenetre, Fe*longueurFenetre*100));
+  signal_fft = signal_fft(1:round(Fe/2));
+  
+  [maxValueSignal, indexOfMaxValueSignal] = max(signal_fft);
+  bpm = (indexOfMaxValueSignal)*0.6; % 60/100 = 0.6
+  disp('Entre le temps t ci-dessus et (t + la longueur de la fenetre)');
+  disp(i);
+  disp('On a en BPM :');
+  disp(bpm);
+  disp(' ');
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%ù
+
+%%% Open_dat.m %%%
+
+function [x,Fe] = Open_dat(filename)
  
 fid=fopen(filename,'r');
 time=10;
